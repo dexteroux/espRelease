5c64fb0649d6fdf0f7480e3ac37623f1ef2968fc
Dexteroux <dexteroux@gmail.com>
2021-11-24 02:47:22 +0000

    Test Bin added

---
+++ espBoot.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import argparse
+import RPi.GPIO as GPIO
+import time
+
+#rpi_pow = 17
+esp_en = 17
+esp_boot = 27
+
+parser = argparse.ArgumentParser(description='Enable Boot mode for programming ESP.')
+parser.add_argument('duration', metavar='N', type=int, nargs='?', default=1,
+                    help='duration in seconds')
+
+args = parser.parse_args()
+duration = args.duration
+
+GPIO.setmode(GPIO.BCM)
+
+#GPIO.setup(rpi_pow, GPIO.OUT) 
+#GPIO.setup(esp_en,   GPIO.OUT) 
+GPIO.setup(esp_boot, GPIO.OUT) 
+
+for i in range(duration):
+    GPIO.output(esp_boot, 0)
+    print(duration - i, "seconds to elapsed ...")
+    time.sleep(1)
+
+
+GPIO.setup(esp_boot, GPIO.IN) 
+GPIO.setup(esp_en, GPIO.OUT) 
+GPIO.output(esp_en, 0)
+time.sleep(3)
+
+GPIO.cleanup()
---
+++ install.sh
@@ -0,0 +1,2 @@
+esptool.py --chip ESP32 -p /dev/ttyS0 -b 460800 --before=no_reset --after=no_reset write_flash --flash_mode dio --flash_freq 40m --flash_size 2MB 0x8000 images/partition_table/partition-table.bin 0x1000 images/bootloader/bootloader.bin 0x10000 images/esp_timer_example.bin
+
