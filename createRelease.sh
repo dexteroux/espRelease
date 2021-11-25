040861b1a526ba82db4b291290cc182e22c5eb9a
Dexteroux <dexteroux@gmail.com>
2021-11-25 23:22:57 +0530

    Firmware updated

---
+++ createRelease.sh
@@ -0,0 +1,4 @@
+cp ~/devel/espAutoLoader/esp_timer/build/esp_timer_example.bin               images/esp_timer_example.bin 
+cp ~/devel/espAutoLoader/esp_timer/build/bootloader/bootloader.bin           images/bootloader/bootloader.bin 
+cp ~/devel/espAutoLoader/esp_timer/build/partition_table/partition-table.bin images/partition_table/partition-table.bin 
+
---
+++ testService.sh
@@ -0,0 +1,1 @@
+sudo -u www-data /usr/bin/python3 /home/www-data/web2py/web2py.py -K client
