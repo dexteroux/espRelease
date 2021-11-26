4407e9263801ff3af8d0be49a4dad6a9e6b898b4
Dexteroux <dexteroux@gmail.com>
2021-11-26 07:51:28 +0530

    Update Client Service

--- installClient.sh
+++ installClient.sh
@@ -1,3 +1,6 @@
 rsync -azP ./client/ /home/www-data/web2py/applications/client
 cp ./routes.py /home/www-data/web2py/
 chown -R www-data:www-data /home/www-data/web2py/applications/client
+cp ./web2pyClientSched.service /etc/systemd/system/web2pyClientSched.service
+systemctl enable web2pyClientSched.service
+systemctl start web2pyClientSched.service
