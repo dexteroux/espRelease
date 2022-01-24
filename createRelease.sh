c5b59d377f6e37b1a7cbc8ffe03a1237122ad6c0
dexter <dexteroux@gmail.com>
2022-01-25 03:14:38 +0530

    update

--- createRelease.sh
+++ createRelease.sh
@@ -4,8 +4,10 @@ cp ~/espAutoLoader/esp_timer/build/partition_table/partition-table.bin images/pa
 cp ~/espAutoLoader/esp_timer/build/ota_data_initial.bin                images/ota_data_initial.bin
 
 gocryptfs ./distribution/cryptfs ./distribution/open
-rsync -azP --exclude "build" /home/pi/espAutoLoader/esp_timer ./distribution/open
-rsync -azP /home/www-data/web2py/applications/client ./distribution/open
-rm -rf ./distribution/open/client/databases/* ./distribution/open/client/errors/* ./distribution/open/client/sessions/*
+pushd ./distribution/open
+rsync -azP --exclude "build" /home/pi/espAutoLoader/esp_timer ./
+rsync -azP /home/www-data/web2py/applications/client ./
+rm -rf ./client/databases/* ./client/errors/* ./client/sessions/*
+popd
 sudo umount ./distribution/open
 
--- installClient.sh
+++ installClient.sh
@@ -1,7 +1,15 @@
+
+gocryptfs ./distribution/cryptfs ./distribution/open
+pushd ./distribution/open
+
 rsync -azP ./client/ /home/www-data/web2py/applications/client
 cp ./routes.py /home/www-data/web2py/
-chown -R www-data:www-data /home/www-data/web2py/applications/client
 cp ./web2pyClientSched.service /etc/systemd/system/web2pyClientSched.service
+
+chown -R www-data:www-data /home/www-data/web2py/applications/client
 systemctl enable web2pyClientSched.service
 systemctl restart web2pyClientSched.service
 systemctl restart apache2.service
+
+popd
+sudo umount ./distribution/open
