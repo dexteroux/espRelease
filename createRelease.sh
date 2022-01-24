db4064a17e09e15307a65b430f596fcd86b3b79b
dexter <dexteroux@gmail.com>
2022-01-25 03:02:43 +0530

    added crypt fs

--- createRelease.sh
+++ createRelease.sh
@@ -1,8 +1,11 @@
-cp ~/devel/espAutoLoader/esp_timer/build/esp_timer_example.bin               images/esp_timer_example.bin 
-cp ~/devel/espAutoLoader/esp_timer/build/bootloader/bootloader.bin           images/bootloader/bootloader.bin 
-cp ~/devel/espAutoLoader/esp_timer/build/partition_table/partition-table.bin images/partition_table/partition-table.bin 
-cp ~/devel/espAutoLoader/esp_timer/build/ota_data_initial.bin                images/ota_data_initial.bin
+cp ~/espAutoLoader/esp_timer/build/esp_timer_example.bin               images/esp_timer_example.bin 
+cp ~/espAutoLoader/esp_timer/build/bootloader/bootloader.bin           images/bootloader/bootloader.bin 
+cp ~/espAutoLoader/esp_timer/build/partition_table/partition-table.bin images/partition_table/partition-table.bin 
+cp ~/espAutoLoader/esp_timer/build/ota_data_initial.bin                images/ota_data_initial.bin
 
-rsync -azP /home/www-data/web2py/applications/client/ ./client
-rm -rf client/databases/* client/errors/* rm -rf client/sessions/*
+gocryptfs ./distribution/cryptfs ./distribution/open
+rsync -azP --exclude "build" /home/pi/espAutoLoader/esp_timer ./distribution/open
+rsync -azP /home/www-data/web2py/applications/client ./distribution/open
+rm -rf ./distribution/open/client/databases/* ./distribution/open/client/errors/* ./distribution/open/client/sessions/*
+sudo umount ./distribution/open
 
