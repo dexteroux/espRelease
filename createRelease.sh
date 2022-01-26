86e7c061ac1369f234fd2e1644d6c16fe5a836a3
deteroux <deteroux@google.com>
2022-01-26 15:25:37 +0530

    update

--- createRelease.sh
+++ createRelease.sh
@@ -1,13 +1,13 @@
-cp ~/espAutoLoader/esp_timer/build/esp_timer_example.bin               images/esp_timer_example.bin 
-cp ~/espAutoLoader/esp_timer/build/bootloader/bootloader.bin           images/bootloader/bootloader.bin 
-cp ~/espAutoLoader/esp_timer/build/partition_table/partition-table.bin images/partition_table/partition-table.bin 
-cp ~/espAutoLoader/esp_timer/build/ota_data_initial.bin                images/ota_data_initial.bin
+cp /home/pi/src/build/esp_timer_example.bin               images/esp_timer_example.bin 
+cp /home/pi/src/build/bootloader/bootloader.bin           images/bootloader/bootloader.bin 
+cp /home/pi/src/build/partition_table/partition-table.bin images/partition_table/partition-table.bin 
+cp /home/pi/src/build/ota_data_initial.bin                images/ota_data_initial.bin
 
 gocryptfs ./distribution/cryptfs ./distribution/open
-pushd ./distribution/open
-rsync -azP --exclude "build" /home/pi/espAutoLoader/esp_timer ./
+cd ./distribution/open
+rsync -azP --exclude "build" /home/pi/src/ ./esp_timer
 rsync -azP /home/www-data/web2py/applications/client ./
 rm -rf ./client/databases/* ./client/errors/* ./client/sessions/*
-popd
+cd ../..
 sudo umount ./distribution/open
 
