baf9ea8d769b5086453416c9d7289ce70285db09
dexteroux <dexteroux@gmail.com>
2022-03-07 09:53:48 +0530

    update

--- testService.sh
+++ testService.sh
@@ -1,2 +1,3 @@
 sudo systemctl stop web2pyClientSched.service
-sudo -u www-data /usr/bin/python3 /home/www-data/web2py/web2py.py -K client
+#sudo -u www-data /usr/bin/python3 /home/www-data/web2py/web2py.py -K client
+sudo -u root /usr/bin/python3 /home/www-data/web2py/web2py.py -K client
