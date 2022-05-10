75744c445aa9bb8b1c29db5033bfb34a84ff3c40
Dexteroux <dexteroux@gmail.com>
2022-05-10 13:05:27 +0530

    update

--- modemManager.sh
+++ modemManager.sh
@@ -1,5 +1,6 @@
 sudo apt install modemmanager network-manager dnsutils ntpstat
 mmcli --list-modems
-sudo nmcli connection add type gsm ifname '*' con-name 'jio' apn 'jionet' gsm.home-only no connection.autoconnect yes ipv6.route-metric 100
+sudo nmcli connection add type gsm ifname '*' con-name 'jio' apn 'jionet' gsm.home-only no connection.autoconnect yes ipv6.route-metric 100 ipv4.route-metric 100
 nmcli dev wifi connect kitaabikeeda_EXT password "anirudhkahaanhai"
-sudo nmcli connection add type ethernet connection.interface-name eth0 connection.id  "Ethernet" ip4 192.168.1.1/24 gw4 192.168.1.10
+sudo nmcli con add con-name eth0 type ethernet ifname eth0 ipv4.method manual ipv4.address 192.168.1.1/24 ipv4.gateway 192.168.1.100 connection.autoconnect yes ipv4.route-metric 900
+
