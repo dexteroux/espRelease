b2b30a4bff053aa1a3cf3e89c61f4b8230dada39
Dexteroux <dexteroux@gmail.com>
2022-07-09 18:48:36 +0530

    Update

--- modemManager.sh
+++ modemManager.sh
@@ -6,6 +6,10 @@ sudo nmcli connection add type gsm ifname '*' con-name 'bsnl' apn 'bsnlstatic' g
 #nmcli dev wifi connect kitaabikeeda_EXT password "anirudhkahaanhai"
 #sudo nmcli dev wifi connect AndroidLink password "12345678" ipv4.route-metric 500 
 sudo nmcli connection add type wifi ssid "AndroidLink" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "12345678" ipv4.route-metric 500 
-sudo nmcli con add con-name eth1 type ethernet ifname eth1 ipv4.method manual ipv4.address 192.168.1.100/24 ipv4.gateway 192.168.1.1 connection.autoconnect yes ipv4.route-metric 200
-sudo nmcli con add con-name eth0 type ethernet ifname eth0 ipv4.method shared ipv4.address 192.168.100.1/24 ipv4.gateway 192.168.100.1 connection.autoconnect yes ipv4.route-metric 900
+#sudo nmcli con add con-name eth1 type ethernet ifname eth1 ipv4.method manual ipv4.address 192.168.1.100/24 ipv4.gateway 192.168.1.1 connection.autoconnect yes ipv4.route-metric 200
+sudo nmcli connection add type wifi ssid "kitaabikeeda_EXT" wifi-sec.key-mgmt wpa-psk wifi-sec.psk "anirudhkahaanhai" ipv4.route-metric 500                                               
+sudo nmcli con add con-name eth1 type ethernet ifname eth1 ipv4.method auto
+sudo nmcli con add con-name eth0 type ethernet ifname eth0 ipv4.method shared ipv4.address 192.168.100.1/24 ipv4.gateway 192.168.100.1 connection.autoconnect yes ipv4.route-metric 900   
+
+
 
